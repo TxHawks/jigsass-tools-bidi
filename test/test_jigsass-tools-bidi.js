6952daa13ab1dc1a167c17f55d8481f78d4c9a8d
TxHawks <tofu.hawks@gmail.com>
2016-03-21 22:29:19 +0200

    Add mixin to handle conversion of border-radius
    Add test for mixin and helper function

--- test/test_jigsass-tools-bidi.js
+++ test/test_jigsass-tools-bidi.js
@@ -11,6 +11,7 @@ describe('jigsass-tools-bidi', () => {
     sassaby.func('jig-var-equals').calledWithArgs('"", $empty');
   });
 
+
   describe('_jigsass-get-side [Function]', () => {
     describe('LTR', () => {
       it('Retruns `left` for `start` when in ltr', () => {
@@ -41,6 +42,7 @@ describe('jigsass-tools-bidi', () => {
       });
     });
   });
+
 
   describe('jigsass-str-replace [Function]', () => {
     it('Replaces a substring in the middle of a sring', () => {
@@ -77,6 +79,179 @@ describe('jigsass-tools-bidi', () => {
       sassaby.func('jigsass-str-replace')
         .calledWithArgs('foo Bar baz', 'bar', 'Quax', '$match-case: false')
         .equals('foo Quax baz');
+    });
+  });
+
+
+  describe('_jigsass-parse-bdrs-values [Function]', () => {
+    it('Return a map with the correct values when only primary values are passed ', () => {
+      sassaby.func('inspect')
+        .calledWithArgs('_jigsass-parse-bdrs-values(12px 8px 9px 10px)')
+        .equals('(primary:12px 8px 9px 10px,secondary:null)');
+    });
+
+    it('Return a map with the correct values when primary values are passed ' +
+       'as a list and secondary values are passed numbers', () => {
+      sassaby.func('inspect')
+        .calledWithArgs('_jigsass-parse-bdrs-values((12px 8px 9px 10px) 15px 18px)')
+        .equals('(primary:12px 8px 9px 10px,secondary:15px 18px)');
+    });
+
+    it('Return a map with the correct values when primary values are passed ' +
+       'as numbers and secondary values are passed as a list', () => {
+      sassaby.func('inspect')
+        .calledWithArgs('_jigsass-parse-bdrs-values(12px 8px 9px 10px (15px 18px))')
+        .equals('(primary:12px 8px 9px 10px,secondary:15px 18px)');
+    });
+
+    it('Return a map with the correct values when a single primary value is passed ' +
+       'as a number and a single secondary value is passed as a list', () => {
+      sassaby.func('inspect')
+        .calledWithArgs('_jigsass-parse-bdrs-values(12px (26px,))')
+        .equals('(primary:12px,secondary:26px)');
+    });
+
+    it('Return a map with the correct values when both primary and secondary ' +
+       'values are passed as lists', () => {
+      sassaby.func('inspect')
+        .calledWithArgs('_jigsass-parse-bdrs-values((12px 8px 9px 10px) (15px 18px))')
+        .equals('(primary:12px 8px 9px 10px,secondary:15px 18px)');
+    });
+  });
+
+
+  describe('_jigsass-bidi-bdrs [Mixin]', () => {
+    describe('LTR', () => {
+      it('prints single value when called with a single value', () => {
+        sassaby.includedMixin('_jigsass-bidi-bdrs')
+          .calledWithArgs('6px', '$rem: false')
+          .equals('border-radius: 6px');
+      });
+
+      it('prints values in the correct order when called with two values', () => {
+        sassaby.includedMixin('_jigsass-bidi-bdrs')
+          .calledWithArgs('6px 12px', '$rem: false')
+          .equals('border-radius: 6px 12px');
+      });
+
+      it('Returns values in rem', () => {
+        sassaby.includedMixin('_jigsass-bidi-bdrs')
+          .calledWithArgs('6px 12px', '$rem: true')
+          .equals('border-radius: 1rem 2rem');
+      });
+
+      it('prints values in the correct order when called with three values', () => {
+        sassaby.includedMixin('_jigsass-bidi-bdrs')
+          .calledWithArgs('8px 12px 6px', '$rem: false')
+          .equals('border-radius: 8px 12px 6px');
+      });
+
+      it('prints values in the correct order when called with four values', () => {
+        sassaby.includedMixin('_jigsass-bidi-bdrs')
+          .calledWithArgs('8px 12px 6px 5px', '$rem: false')
+          .equals('border-radius: 8px 12px 6px 5px');
+      });
+    });
+
+    describe('RTL', () => {
+      const sassaby = new Sassaby(file, { variables: { 'jigsass-direction': 'rtl', } })
+
+      it('prints single value when called with a single value', () => {
+        sassaby.includedMixin('_jigsass-bidi-bdrs')
+          .calledWithArgs('6px', '$rem: false')
+          .equals('border-radius: 6px');
+      });
+
+      it('prints values in the correct order when called with two values', () => {
+        sassaby.includedMixin('_jigsass-bidi-bdrs')
+          .calledWithArgs('6px 12px', '$rem: false')
+          .equals('border-radius: 12px 6px');
+      });
+
+      it('Returns values in rem', () => {
+        sassaby.includedMixin('_jigsass-bidi-bdrs')
+          .calledWithArgs('6px 12px', '$rem: true')
+          .equals('border-radius: 2rem 1rem');
+      });
+
+      it('prints values in the correct order when called with three values', () => {
+        sassaby.includedMixin('_jigsass-bidi-bdrs')
+          .calledWithArgs('8px 12px 6px', '$rem: false')
+          .equals('border-radius: 12px 8px 12px 6px');
+      });
+
+      it('prints values in the correct order when called with four values', () => {
+        sassaby.includedMixin('_jigsass-bidi-bdrs')
+          .calledWithArgs('8px 12px 6px 5px', '$rem: false')
+          .equals('border-radius: 12px 8px 5px 6px');
+      });
+    });
+
+    describe('Secondary (vertical) values', () => {
+      describe('LTR', () => {
+        it('prints single secondary value when a single value is passed', () => {
+          sassaby.includedMixin('_jigsass-bidi-bdrs')
+            .calledWithArgs('128px (6px,)', '$rem: false')
+            .equals('border-radius: 128px  /  6px');
+        });
+
+        it('prints values in the correct order when called with two secondary values', () => {
+          sassaby.includedMixin('_jigsass-bidi-bdrs')
+            .calledWithArgs('6px (12px 18px)', '$rem: false')
+            .equals('border-radius: 6px  /  12px 18px');
+        });
+
+        it('Returns values in rem', () => {
+          sassaby.includedMixin('_jigsass-bidi-bdrs')
+            .calledWithArgs('6px 12px (12px 6px)', '$rem: true')
+            .equals('border-radius: 1rem 2rem  /  2rem 1rem');
+        });
+
+        it('prints values in the correct order when called with three secondary values', () => {
+          sassaby.includedMixin('_jigsass-bidi-bdrs')
+            .calledWithArgs('8px (12px 6px 18px)', '$rem: false')
+            .equals('border-radius: 8px  /  12px 6px 18px');
+        });
+
+        it('prints values in the correct order when called with four secondary values', () => {
+          sassaby.includedMixin('_jigsass-bidi-bdrs')
+            .calledWithArgs('8px (12px 6px 5px 18px)', '$rem: false')
+            .equals('border-radius: 8px  /  12px 6px 5px 18px');
+        });
+      });
+      describe('RTL', () => {
+        const sassaby = new Sassaby(file, { variables: { 'jigsass-direction': 'rtl', } })
+
+        it('prints single secondary value when a single value is passed', () => {
+          sassaby.includedMixin('_jigsass-bidi-bdrs')
+            .calledWithArgs('128px (6px,)', '$rem: false')
+            .equals('border-radius: 128px  /  6px');
+        });
+
+        it('prints values in the correct order when called with two secondary values', () => {
+          sassaby.includedMixin('_jigsass-bidi-bdrs')
+            .calledWithArgs('6px (18px 12px)', '$rem: false')
+            .equals('border-radius: 6px  /  12px 18px');
+        });
+
+        it('Returns values in rem', () => {
+          sassaby.includedMixin('_jigsass-bidi-bdrs')
+            .calledWithArgs('6px 12px (12px 6px)', '$rem: true')
+            .equals('border-radius: 2rem 1rem  /  1rem 2rem');
+        });
+
+        it('prints values in the correct order when called with three secondary values', () => {
+          sassaby.includedMixin('_jigsass-bidi-bdrs')
+            .calledWithArgs('8px (12px 6px 18px)', '$rem: false')
+            .equals('border-radius: 8px  /  6px 12px 6px 18px');
+        });
+
+        it('prints values in the correct order when called with four secondary values', () => {
+          sassaby.includedMixin('_jigsass-bidi-bdrs')
+            .calledWithArgs('8px (12px 6px 5px 18px)', '$rem: false')
+            .equals('border-radius: 8px  /  6px 12px 18px 5px');
+        });
+      });
     });
   });
 });
